2525e68f2c8bbbde77cdc067b71851a9c26dadd3
madhuka <madhukaudantha@gmail.com>
2013-11-27 11:04:04 +0530

    Changing file Structure for JS from Jag (request on 11/26/2013) Front End controller need to be build

---
+++ testingApp/automobileJS/test/car/toyota/carSpec.js
@@ -0,0 +1,56 @@
+//define test suites 
+
+var Car = require('../../modules/car.js').Car;
+
+test.describe('TestSuiteTwo-Toyota', function() {
+
+	//config data for helping to test
+	var myCar,pravinCar = null,myCarConfig = {name:"Vitz", engineSize: 1000},
+	pravinCarConfig = {name:"Prius", engineSize: 1800};
+	
+	//setting and define object to testing 
+	test.beforeEach(function() {		
+		myCar = new Car(myCarConfig);
+		pravinCar = new Car(pravinCarConfig);
+	});
+	
+	//First test for to check was car built fine. 
+	test.it('Testing Car name and Engineer size is validate to Drive', function() {
+		test.expect(myCar.name).toEqual("Vitz");
+		test.expect(myCar.engineSize).toEqual(1000);
+	});
+
+
+	//grouping test for Engine Turning Test
+	test.describe("TurningEngineForToyotaCar", function() {
+
+	//checking test platform work fine
+	test.it("Checking testing platform is turned to test", function() {
+		test.expect(1).toEqual(1);
+	});
+					
+	//testing car (function) racing by enginee
+	test.it("Checking Engineer capacity for Express way to ride", function() {
+		test.expect(myCar.getEngineSize()).toEqual(1000);
+	});
+						
+	});
+	
+	test.describe("upgradeEngineSizeOfToyotacar", function() {
+	
+
+		test.it("Checking testing platform is turned to test", function() {
+			test.expect(1).toBe(1);
+		});
+						
+
+		test.it("upgradeEngineSize to testing", function() {
+			test.expect(myCar.upgradeEngineSize()).toBe(1500);
+			test.expect(pravinCar.upgradeEngineSize()).toBe(2300);
+			
+		});
+							
+		});
+
+});
+
