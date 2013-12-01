beb847010f654371e05a7c626b50baf43dfd7258
madhuka <madhukaudantha@gmail.com>
2013-12-01 19:02:36 +0530

    Coding formating upgrade (From Devs to standard formated according to by Einar Lielmanis) http://jsbeautifier.org/

--- module/test/scripts/engine/cola.js
+++ module/test/scripts/engine/cola.js
@@ -1,300 +1,298 @@
-var cola = (function() {
+var cola = (function () {
 
-	// for test specification name identifyer
-	var specifcation = null;
+    // for test specification name identifyer
+    var specifcation = null;
 
-	//test spec extenstion
-	var TEST_FILE_EXTENSIOIN = '.js';
+    //test spec extenstion
+    var TEST_FILE_EXTENSIOIN = '.js';
 
-	jasmine.Spec.prototype.runs = function(func) {
-		var block = new jasmine.Block(this.env, func, this), spec = specifcation;
-		log.debug('specifcation is found ' + spec);
-		// replace to id for this.env.currentSpec.suite.description
+    jasmine.Spec.prototype.runs = function (func) {
+        var block = new jasmine.Block(this.env, func, this),
+            spec = specifcation;
+        log.debug('specifcation is found ' + spec);
+        // replace to id for this.env.currentSpec.suite.description
 
-		if (spec == null) {
-			this.addToQueue(block);			
-		} else if (this.env.currentSpec.suite.queue.env.currentSpec.id == spec) {
-			this.addToQueue(block);
-			
-		}
-		
-		return this;
-	};
+        if (spec == null) {
+            this.addToQueue(block);
+        } else if (this.env.currentSpec.suite.queue.env.currentSpec.id == spec) {
+            this.addToQueue(block);
 
-	jasmine.Queue.prototype.next_ = function() {
-		var self = this;
-		var goAgain = true;
+        }
 
-		while (goAgain) {
-			goAgain = false;
+        return this;
+    };
 
-			if (self.index < self.blocks.length
-					&& !(this.abort && !this.ensured[self.index])) {
-				var calledSynchronously = true;
-				var completedSynchronously = false;
+    jasmine.Queue.prototype.next_ = function () {
+        var self = this;
+        var goAgain = true;
 
-				var onComplete = function() {
-					if (jasmine.Queue.LOOP_DONT_RECURSE && calledSynchronously) {
-						completedSynchronously = true;
-						return;
-					}
+        while (goAgain) {
+            goAgain = false;
 
-					if (self.blocks[self.index].abort) {
-						self.abort = true;
-					}
+            if (self.index < self.blocks.length && !(this.abort && !this.ensured[self.index])) {
+                var calledSynchronously = true;
+                var completedSynchronously = false;
 
-					self.offset = 0;
-					self.index++;
+                var onComplete = function () {
+                    if (jasmine.Queue.LOOP_DONT_RECURSE && calledSynchronously) {
+                        completedSynchronously = true;
+                        return;
+                    }
 
-					var now = new Date().getTime();
+                    if (self.blocks[self.index].abort) {
+                        self.abort = true;
+                    }
 
-					if (self.env.lastUpdate == 0) {
+                    self.offset = 0;
+                    self.index++;
 
-						self.env.lastUpdate = now;
-					}
+                    var now = new Date().getTime();
 
-					if (self.env.updateInterval
-							&& now - self.env.lastUpdate > self.env.updateInterval) {
-						self.env.lastUpdate = now;
-						self.next_();
-					} else {
-						if (jasmine.Queue.LOOP_DONT_RECURSE
-								&& completedSynchronously) {
+                    if (self.env.lastUpdate == 0) {
 
-							goAgain = true;
-						} else {
-							self.next_();
-						}
-					}
-				};
-				self.blocks[self.index].execute(onComplete);
+                        self.env.lastUpdate = now;
+                    }
 
-				calledSynchronously = false;
-				if (completedSynchronously) {
-					onComplete();
-				}
+                    if (self.env.updateInterval && now - self.env.lastUpdate > self.env.updateInterval) {
+                        self.env.lastUpdate = now;
+                        self.next_();
+                    } else {
+                        if (jasmine.Queue.LOOP_DONT_RECURSE && completedSynchronously) {
 
-			} else {
-				self.running = false;
-				if (self.onComplete) {
-					self.onComplete();
-				}
-			}
-		}
-	};
+                            goAgain = true;
+                        } else {
+                            self.next_();
+                        }
+                    }
+                };
+                self.blocks[self.index].execute(onComplete);
 
-	/*
-	 * function execute calling runner to starting testing after identtifiying
-	 * url and location pattern to be executed
-	 * 
-	 */
-	jasmine.Env.prototype.execute = function() {
-		if (urlMapper()) {
-			this.currentRunner_.execute();
-		}
-	};
+                calledSynchronously = false;
+                if (completedSynchronously) {
+                    onComplete();
+                }
 
-	/**
-	 * function have URl mappering for pattern unlimate folder strure with one
-	 * file inside with specfication
-	 * 
-	 * http://localhost:9763/automobile/test[/{folderName}0-n][/{fileName}0-1][/{
-	 * testSpecName }0-1] * above URL pattern can be handle
-	 * 
-	 */
-	urlMapper = function() {
-		var uri = request.getRequestURI(), pathMatcher1 = null, pathMatcher2 = null, uriMatcher = new URIMatcher(
-				uri);
-		log.debug(uri);
+            } else {
+                self.running = false;
+                if (self.onComplete) {
+                    self.onComplete();
+                }
+            }
+        }
+    };
 
-		// Provide a pattern to be matched against the URL
-		if (uriMatcher.match('/{appname}/{test}/{+path}')) {
-			// If pattern matches, elements can be accessed from their keys
+    /*
+     * function execute calling runner to starting testing after identtifiying
+     * url and location pattern to be executed
+     *
+     */
+    jasmine.Env.prototype.execute = function () {
+        if (urlMapper()) {
+            this.currentRunner_.execute();
+        }
+    };
 
-			pathMatcher1 = '/' + uriMatcher.elements().test + '/'
-					+ uriMatcher.elements().path;
-			if (pathMatcher1 != null) {
-				pathMatcher2 = pathMatcher1.substr(0, pathMatcher1
-						.lastIndexOf("/"));
-				if (isValidPath(pathMatcher1, pathMatcher2)) {
-					specifcation = pathMatcher1.substr(pathMatcher1
-							.lastIndexOf("/") + 1);
-				}
-			}
-			log.debug('path : ' + pathMatcher1 + ',' + pathMatcher2);
-			return validatedPatten(pathMatcher1, pathMatcher2);
-			log.debug(isExists(pathMatcher1));
-			log.debug(isExists(pathMatcher2));
+    /**
+     * function have URl mappering for pattern unlimate folder strure with one
+     * file inside with specfication
+     *
+     * http://localhost:9763/automobile/test[/{folderName}0-n][/{fileName}0-1][/{
+     * testSpecName }0-1] * above URL pattern can be handle
+     *
+     */
+    urlMapper = function () {
+        var uri = request.getRequestURI(),
+            pathMatcher1 = null,
+            pathMatcher2 = null,
+            uriMatcher = new URIMatcher(
+                uri);
+        log.debug(uri);
 
-		}
+        // Provide a pattern to be matched against the URL
+        if (uriMatcher.match('/{appname}/{test}/{+path}')) {
+            // If pattern matches, elements can be accessed from their keys
 
-		if (uriMatcher.match('/{appname}/{test}/')) {
-			// root pattern for test Dir
-			log.debug('getting all file from Require');
-			crawl('/' + uriMatcher.elements().test);
-			return true;
-		}
+            pathMatcher1 = '/' + uriMatcher.elements().test + '/' + uriMatcher.elements().path;
+            if (pathMatcher1 != null) {
+                pathMatcher2 = pathMatcher1.substr(0, pathMatcher1
+                    .lastIndexOf("/"));
+                if (isValidPath(pathMatcher1, pathMatcher2)) {
+                    specifcation = pathMatcher1.substr(pathMatcher1
+                        .lastIndexOf("/") + 1);
+                }
+            }
+            log.debug('path : ' + pathMatcher1 + ',' + pathMatcher2);
+            return validatedPatten(pathMatcher1, pathMatcher2);
+            log.debug(isExists(pathMatcher1));
+            log.debug(isExists(pathMatcher2));
 
-	};
+        }
 
-	/**
-	 * function is validate url pattern for requrieiung files
-	 * 
-	 * @param pathMatcher1
-	 *            is path of a pattern of path can exsting
-	 * @param pathMatcher2
-	 *            is path of a pattern of path can exsting for second level
-	 * @returns boolean
-	 */
-	var validatedPatten = function(pathMatcher1, pathMatcher2) {
-		if (!reqiureFiles(pathMatcher1)) {
-			if (!(reqiureFiles(pathMatcher2)) && pathMatcher2 != null
-					&& !(isValidPath(pathMatcher1, pathMatcher2))) {
-				log.debug("Path not exsting");
-				print({
-					'error' : true,
-					'message' : 'path \'' + pathMatcher1 + '\' is not valid'
-				});
-				return false;
-			}
-		}
-		return true;
-	};
+        if (uriMatcher.match('/{appname}/{test}/')) {
+            // root pattern for test Dir
+            log.debug('getting all file from Require');
+            crawl('/' + uriMatcher.elements().test);
+            return true;
+        }
 
-	/**
-	 * function will reuire all test spec js files/file in path
-	 * 
-	 * @param path
-	 *            can be dir or to file path with out extension all test spec is
-	 *            defin as .js
-	 * @returns
-	 */
-	var reqiureFiles = function(path) {
-		log.debug('requiring from ' + path);
-		isCompleted = false;
-		if (isDirectory(path)) {
-			isCompleted = true;
-			log.debug("is Dir " + path);
-			crawl(path);
-		} else if (isExists(path + TEST_FILE_EXTENSIOIN)) {
-			isCompleted = true;
-			log.debug("is File " + path + TEST_FILE_EXTENSIOIN);
-			require(path + TEST_FILE_EXTENSIOIN);
-		}
-		return isCompleted;
-	};
+    };
 
-	/**
-	 * function crawl will require all the file in root location
-	 * 
-	 * @param root
-	 *            is path of dir to search for test spec files
-	 */
-	var crawl = function(root) {
-		log.debug('crawling on root called ' + root);
-		var file = new File(root), list = file.listFiles();
+    /**
+     * function is validate url pattern for requrieiung files
+     *
+     * @param pathMatcher1
+     *            is path of a pattern of path can exsting
+     * @param pathMatcher2
+     *            is path of a pattern of path can exsting for second level
+     * @returns boolean
+     */
+    var validatedPatten = function (pathMatcher1, pathMatcher2) {
+        if (!reqiureFiles(pathMatcher1)) {
+            if (!(reqiureFiles(pathMatcher2)) && pathMatcher2 != null && !(isValidPath(pathMatcher1, pathMatcher2))) {
+                log.debug("Path not exsting");
+                print({
+                    'error': true,
+                    'message': 'path \'' + pathMatcher1 + '\' is not valid'
+                });
+                return false;
+            }
+        }
+        return true;
+    };
 
-		if (list == null)
-			return;
+    /**
+     * function will reuire all test spec js files/file in path
+     *
+     * @param path
+     *            can be dir or to file path with out extension all test spec is
+     *            defin as .js
+     * @returns
+     */
+    var reqiureFiles = function (path) {
+        log.debug('requiring from ' + path);
+        isCompleted = false;
+        if (isDirectory(path)) {
+            isCompleted = true;
+            log.debug("is Dir " + path);
+            crawl(path);
+        } else if (isExists(path + TEST_FILE_EXTENSIOIN)) {
+            isCompleted = true;
+            log.debug("is File " + path + TEST_FILE_EXTENSIOIN);
+            require(path + TEST_FILE_EXTENSIOIN);
+        }
+        return isCompleted;
+    };
 
-		for ( var i = 0; i < list.length; i++) {
-			log.debug(i + ' checking files on ' + root);
-			var f = list[i];
-			if (f.isDirectory()) {
+    /**
+     * function crawl will require all the file in root location
+     *
+     * @param root
+     *            is path of dir to search for test spec files
+     */
+    var crawl = function (root) {
+        log.debug('crawling on root called ' + root);
+        var file = new File(root),
+            list = file.listFiles();
 
-				log.debug("Dir:" + f.getName());
-				crawl(root + '/' + f.getName());
-			} else {
-				log.debug("File:" + f.getName());
-				require(root + '/' + f.getName());
-			}
-		}
-	};
-	
-	/**
-	 * This function is main for passing js fils in front end
-	 * from from jaggery module level to app level
-	 * TO-DO - TO-CALL - Jaggery have limitation on this, this need to be fixed in jaggery
-	 * THIS-LIMITATION in jaggery not aollowing AJAX process in front end
-	 * 
-	 * @param path of the file inside jaggery module
-	 * @returns file will be return for front end 
-	 */
-	var loadJSToFront = function(filePath) {
-		var file = new File(filePath);
-		file.open("r");
-		print(file.readAll());
-	};
-	
+        if (list == null)
+            return;
 
-	/**
-	 * run is main function that will be called from app test level
-	 * this funtion will register the reporter for ENV in jasmine
-	 * 
-	 */
-	var run = function() {
-		log.debug('Called the run for cola');
-		var jasmineEnv = jasmine.getEnv();
-		var jsonReporter = new jasmine.JSONReporter();
-		jasmineEnv.addReporter(jsonReporter);
-		jasmineEnv.execute();
-	};
-	
-	
-	/**
-	 * function is to checks whether the given path is a directory
-	 * 
-	 * @param path
-	 * @returns directory is esxting return true
-	 */
-	var isDirectory = function (path) {
-		var file = new File(path);
-		return file.isDirectory();
-	};
-	
+        for (var i = 0; i < list.length; i++) {
+            log.debug(i + ' checking files on ' + root);
+            var f = list[i];
+            if (f.isDirectory()) {
 
-	/**
-	 * file give the name of the file 
-	 * @param path
-	 * @returns Name of the file without the path
-	 */
-	var getName = function(path) {
-		var file = new File(path);		
-		return file.getName();
-	};
+                log.debug("Dir:" + f.getName());
+                crawl(root + '/' + f.getName());
+            } else {
+                log.debug("File:" + f.getName());
+                require(root + '/' + f.getName());
+            }
+        }
+    };
 
-	/**
-	 * 
-	 * @param path
-	 * @returns checks whether this file actually exists. Returns true if the
-	 *          file exists.
-	 */
-	var isExists = function(path) {
-		var file = new File(path);
-		return file.isExists();
-	};
+    /**
+     * This function is main for passing js fils in front end
+     * from from jaggery module level to app level
+     * TO-DO - TO-CALL - Jaggery have limitation on this, this need to be fixed in jaggery
+     * THIS-LIMITATION in jaggery not aollowing AJAX process in front end
+     *
+     * @param path of the file inside jaggery module
+     * @returns file will be return for front end
+     */
+    var loadJSToFront = function (filePath) {
+        var file = new File(filePath);
+        file.open("r");
+        print(file.readAll());
+    };
 
-	/**
-	 * validation check for spec name of test suite
-	 * 
-	 * @param path1
-	 * @param path2
-	 * @returns {Boolean}
-	 */
-	var isValidPath = function(path1, path2) {
-		var isValid = false;
-		if ((isDirectory(path2) || isExists(path2 + TEST_FILE_EXTENSIOIN))
-				&& !(isDirectory(path1) || isExists(path1
-						+ TEST_FILE_EXTENSIOIN))) {
-			log.debug('Current URL - path is validated for testspec name.');
-			isValid = true;
-		}
-		return isValid;
-	};
 
-	return {
-		run : run,
-		loadJSToFront : loadJSToFront
-	};
+    /**
+     * run is main function that will be called from app test level
+     * this funtion will register the reporter for ENV in jasmine
+     *
+     */
+    var run = function () {
+        log.debug('Called the run for cola');
+        var jasmineEnv = jasmine.getEnv();
+        var jsonReporter = new jasmine.JSONReporter();
+        jasmineEnv.addReporter(jsonReporter);
+        jasmineEnv.execute();
+    };
+
+
+    /**
+     * function is to checks whether the given path is a directory
+     *
+     * @param path
+     * @returns directory is esxting return true
+     */
+    var isDirectory = function (path) {
+        var file = new File(path);
+        return file.isDirectory();
+    };
+
+
+    /**
+     * file give the name of the file
+     * @param path
+     * @returns Name of the file without the path
+     */
+    var getName = function (path) {
+        var file = new File(path);
+        return file.getName();
+    };
+
+    /**
+     *
+     * @param path
+     * @returns checks whether this file actually exists. Returns true if the
+     *          file exists.
+     */
+    var isExists = function (path) {
+        var file = new File(path);
+        return file.isExists();
+    };
+
+    /**
+     * validation check for spec name of test suite
+     *
+     * @param path1
+     * @param path2
+     * @returns {Boolean}
+     */
+    var isValidPath = function (path1, path2) {
+        var isValid = false;
+        if ((isDirectory(path2) || isExists(path2 + TEST_FILE_EXTENSIOIN)) && !(isDirectory(path1) || isExists(path1 + TEST_FILE_EXTENSIOIN))) {
+            log.debug('Current URL - path is validated for testspec name.');
+            isValid = true;
+        }
+        return isValid;
+    };
+
+    return {
+        run: run,
+        loadJSToFront: loadJSToFront
+    };
 
 }());
--- module/test/scripts/report/jsonReporter.js
+++ module/test/scripts/report/jsonReporter.js
@@ -1,77 +1,74 @@
-jasmine.JSONReporter = function() {
+jasmine.JSONReporter = function () {
 
-	var startingTime = (new Date()).getTime(), 
-	log = new Log('jsonReporter');
-	this.exports = {
-		elapsedTime : null,
-		specsCount : 0,
-		specsPassed : 0,
-		specsFailed : 0,
-		suites : []
-	},
+    var startingTime = (new Date()).getTime(),
+        log = new Log('jsonReporter');
+    this.exports = {
+        elapsedTime: null,
+        specsCount: 0,
+        specsPassed: 0,
+        specsFailed: 0,
+        suites: []
+    },
 
-	/*
-	 * reportRunnerResults will called finaly for to get results
-	 */
-	this.reportRunnerResults = function() {
-		this.exports.elapsedTime = parseInt((new Date()).getTime()
-				- startingTime, 10)
-				+ "ms";
-		var outJson = JSON.stringify(this.exports);
-		log.debug(outJson);
-		print(outJson);		
-		if (this.exports.specsCount - this.exports.specsPassed !== 0) {
-			this.loged('Exiting with errors');
-		} else {
-			this.loged('Exiting with ' + this.exports.specsCount
-					+ ' passed specs ');
-		}
+    /*
+     * reportRunnerResults will called finaly for to get results
+     */
+    this.reportRunnerResults = function () {
+        this.exports.elapsedTime = parseInt((new Date()).getTime() - startingTime, 10) + "ms";
+        var outJson = JSON.stringify(this.exports);
+        log.debug(outJson);
+        print(outJson);
+        if (this.exports.specsCount - this.exports.specsPassed !== 0) {
+            this.loged('Exiting with errors');
+        } else {
+            this.loged('Exiting with ' + this.exports.specsCount + ' passed specs ');
+        }
 
-	},
+    },
 
-	/**
-	 * function is result building for each spec
-	 * @param spec is testSpec
-	 */
-	this.reportSpecResults = function(spec) {
-		
-		this.exports.specsCount += 1;
+    /**
+     * function is result building for each spec
+     * @param spec is testSpec
+     */
+    this.reportSpecResults = function (spec) {
 
-		if (spec.results().passed()) {
-			this.exports.specsPassed++;
-		} else {
-			this.exports.specsFailed++;
-		}
+        this.exports.specsCount += 1;
 
-		var l = spec.results().items_.length;
-		var items = new Array();
-		for ( var i = 0; i < l; i++) {
-			log.debug(spec.results().items_[i]);
-			items[i] = {
-				type : spec.results().items_[i].type,
-				message : spec.results().items_[i].message
-			};
-		}
-		// log.info(spec.results());
-		// log.info(spec.suite.toSource());
-		this.exports.suites.push({
-			suite : spec.suite.description,
-			spec : spec.description,
-			passed : spec.results().passed(),
-			itemCount : spec.results().totalCount,
-			passedCount : spec.results().passedCount,
-			failedCount : spec.results().failedCount,
-			skipped : spec.results().skipped,
-			items : items
+        if (spec.results().passed()) {
+            this.exports.specsPassed++;
+        } else {
+            this.exports.specsFailed++;
+        }
 
-		});
-	};
+        var l = spec.results().items_.length;
+        var items = new Array();
+        for (var i = 0; i < l; i++) {
+            log.debug(spec.results().items_[i]);
+            items[i] = {
+                type: spec.results().items_[i].type,
+                message: spec.results().items_[i].message
+            };
+        }
+        // log.info(spec.results());
+        // log.info(spec.suite.toSource());
+        this.exports.suites.push({
+            suite: spec.suite.description,
+            spec: spec.description,
+            passed: spec.results().passed(),
+            itemCount: spec.results().totalCount,
+            passedCount: spec.results().passedCount,
+            failedCount: spec.results().failedCount,
+            skipped: spec.results().skipped,
+            items: items
 
-	/**
-	 * Will be used to logged in finally 
-	 * @param arguments String message to passing for log
-	 */
-	this.loged = function(arguments) {
-		 log.info(arguments);
-	};
+        });
+    };
+
+    /**
+     * Will be used to logged in finally
+     * @param arguments String message to passing for log
+     */
+    this.loged = function (arguments) {
+        log.info(arguments);
+    };
 };
